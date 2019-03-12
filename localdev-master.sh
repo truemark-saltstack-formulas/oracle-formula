f2824f4bf15abd4209bcfbd2eeb01f14780f53c2
Erik Jensen <ejensen@netradius.com>
2019-03-12 17:19:46 -0600

    Adding additional changes for localdev master setup

--- localdev-master.sh
+++ localdev-master.sh
@@ -4,6 +4,8 @@
 # To execute this script run the following:
 # bash < <(curl -s https://raw.githubusercontent.com/truemark-saltstack-formulas/oracle-formula/master/localdev-master.sh)
 
+# Mount salt-oracle directory
+
 tee /etc/rc.local <<EOF
 #!/usr/bin/env bash
 vmhgfs-fuse .host:/\$(vmware-hgfsclient) /srv -o uid=0 -o gid=0 -o umask=0027
@@ -14,15 +16,24 @@ sleep 5
 
 sh /etc/rc.local
 
+# Setup hosts entry for the local salt minion
+
 sed -i -e 's/localhost/localhost salt/'  /etc/hosts
 
-cd /root
+# Allow user to do root stuff without a password
 
+echo 'user ALL=(ALL:ALL) NOPASSWD:ALL' > /etc/sudoers.d/ubuntu
+
+# Install SaltStack
+
+cd /root
 curl -L https://bootstrap.saltstack.com -o install_salt.sh
 sh install_salt.sh -M stable
+rm -f install_salt.sh
 
 service salt-minion restart
 
+# Accept the key for the local minion
 output=$(salt-key -Ay --no-color)
 echo $output
 while [[ "${output}" == *"does not match"* ]]; do
@@ -30,6 +41,7 @@ while [[ "${output}" == *"does not match"* ]]; do
     echo $output
 done
 
+# Do some initial configuration for the salt master
 tee /etc/salt/master.d/ext_pillar.conf <<EOF
 ext_pillar:
   - file_tree:
@@ -46,6 +58,11 @@ file_roots:
     - /srv/salt
     - /srv/formulas/oracle-formula
     - /srv/formulas/proservices-formula
+EOF
+
+tee /srv/salt/top.sls <<EOF
+'$(hostname -f)'
+  -
 EOF
 
 service salt-master restart
