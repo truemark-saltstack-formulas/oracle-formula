dcb4581635dc666575649941fc311bfaba441c9d
Erik Jensen <ejensen@netradius.com>
2019-03-12 11:55:34 -0600

    Adding setup for master localdev

--- localdev-master.sh
+++ localdev-master.sh
@@ -21,3 +21,31 @@ cd /root
 curl -L https://bootstrap.saltstack.com -o install_salt.sh
 sh install_salt.sh -M stable
 
+service salt-minion restart
+
+output=$(salt-key -Ay --no-color)
+echo $output
+while [[ "${output}" == *"does not match"* ]]; do
+    output=$(salt-key -Ay --no-color)
+    echo $output
+done
+
+tee /etc/salt/master.d/ext_pillar.conf <<EOF
+ext_pillar:
+  - file_tree:
+      root_dir: /srv/ext_pillar
+      follow_dir_links: False
+      keep_newline: True
+      template: True
+      render_default: jinja|yaml
+EOF
+
+tee /etc/salt/master.d/file_roots.conf <<EOF
+file_roots:
+  base:
+    - /srv/salt
+    - /srv/formulas/oracle-formula
+    - /srv/formulas/proservices-formula
+EOF
+
+service salt-master restart
