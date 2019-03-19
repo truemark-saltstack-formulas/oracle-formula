d128e5a3d2fe3f3d035c60ce8e44d7634da83476
Erik Jensen <ejensen@netradius.com>
2019-03-18 19:07:46 -0600

    Fixing exit issue in runInstallerWrapper.sh

--- tmoracle/homes/files/runInstallerWrapper.sh
+++ tmoracle/homes/files/runInstallerWrapper.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-set -uex
-
 INSTALLER="${1}"
 RESPONSE_FILE="${2}"
 
 ${INSTALLER} -waitForCompletion -silent -responseFile ${RESPONSE_FILE}
+RETCODE=$?
+
+if [[ "${RETCODE}" == 6 ]]; then
+  exit 0
+fi
+exit ${RETCODE}
